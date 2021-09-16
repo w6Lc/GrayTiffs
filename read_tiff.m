2b77d3ed2f7cb2fa4a8cf8f6c8a6ddc3fad4fef3
wielice <wielice@outlook.com>
2021-09-17 02:12:43 +0800

    添加注释

--- read_tiff.m
+++ read_tiff.m
@@ -1,4 +1,10 @@
 function img = read_tiff(img_file)
+%	Read tiff of 2d or 3d
+%   Args:
+%       img_file (str): tiff of 2d or 3d
+%   Returns:
+%       img (num): 2d or 3d mat
+%   不要直接运行本文件，否则返回值 img 直接输出在控制台
 %% INPUT
 if nargin == 0
     img_file = 'Test/test.tif';
@@ -13,4 +19,6 @@ img = zeros(high, wide, deep);
 for i = 1:deep
     img(:,:,i) = imread(img_file, i);
 end
+
+if nargin == 0, figure, imshow(img(:,:,1), []); end
 end
--- save_tiff.m
+++ save_tiff.m
@@ -1,28 +1,33 @@
 function save_tiff(img, img_file)   
-% 此tiff文件，不支持imwrite的append模式
+%   Save img stacks to tiff of float32
+%   Args:
+%       img(num):  2d or 3d mat
+%       img_file(str): file to save
+%   可以直接运行本文件进行写入读取时间测试
+%   生成的tiff文件，不支持使用imwrite的append模式继续写入
 %% INPUT
 if nargin == 0
-    img = imread('rice.png');
-    img = repmat(img, [1,1,100]);
+    img = repmat(imread('rice.png'), [1,1,100]);
     img_file = 'Test/test.tif';
-    debug = 1;
-else
-    debug = 0;
 end
-%%
-tf_tag.Photometric = Tiff.Photometric.MinIsBlack;
+%% define tiff tag
+if nargin==0, debug=1; else, debug=0; end
+
+% ==============================测试压缩方式================================
 % 测试写入时间，None 0.22, PackBits 0.38, LZW 0.56, Deflate 3.97, png 0.99
 % 测试读取时间，None 0.22, PackBits 0.22, LZW 0.38, Deflate 0.30, png 0.23
 % 测试写入大小，None 25  , PackBits 25  , LZW 7   , Deflate 6   , png    4
 % 时间考虑 None最好， 大小速度综合考虑 pb,lzw都行，大小考虑 png
 tf_tag.Compression = Tiff.Compression.None;   % packBits 基本等同不压缩
+% =========================================================================
+tf_tag.Photometric = Tiff.Photometric.MinIsBlack;
 tf_tag.BitsPerSample = 32;    % 数据类型大小
 tf_tag.SamplesPerPixel = 1;   % 几个通道
 tf_tag.SampleFormat = Tiff.SampleFormat.IEEEFP;   % 数据类型为浮点数
 tf_tag.ImageLength = size(img, 1);
 tf_tag.ImageWidth = size(img, 2);
 tf_tag.PlanarConfiguration = Tiff.PlanarConfiguration.Chunky;
-
+%% write tiff
 if debug, tic; end
 tf = Tiff(img_file,'w');
 for i = 1 : size(img, 3)
@@ -36,7 +41,7 @@ if debug, disp(['写入时间: ', num2str(toc), 's']); end
 % 测试时间
 if debug
     tic
-    for i = 1:100
+    for i = 1:size(img, 3)
         imread(img_file, i);
     end
     disp(['读取时间: ', num2str(toc), 's']);
